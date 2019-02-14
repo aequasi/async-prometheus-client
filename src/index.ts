47957b0b80d8582e777cec8bf95cd519f1347bc4
Aaron Scherer <aequasi@gmail.com>
2019-02-14 01:26:46 -0800

    fix(index): Adding

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import * as Adapter from './Adapter';
+import * as Metrics from './Metric';
+
 export {default as CollectorRegistry} from './CollectorRegistry';
 export {default as Renderer} from './Renderer';
 export {default as InMemoryAdapter} from './Adapter/InMemoryAdapter';
 export {default as RedisAdapter} from './Adapter/RedisAdapter';
-
-import * as Adapter from './Adapter';
-import * as Metrics from './Metric';
 
 export {
     Metrics,
